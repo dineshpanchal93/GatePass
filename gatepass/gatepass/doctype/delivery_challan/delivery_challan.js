0a0bfe425257c8999a5ec21c462e85341c2bf476
nandkishor48 <nandkishorjadhao05@gmail.com>
2023-08-30 21:05:37 +0530

    delivery challan doctype

--- gatepass/gatepass/doctype/delivery_challan/delivery_challan.js
+++ gatepass/gatepass/doctype/delivery_challan/delivery_challan.js
@@ -2,7 +2,48 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Delivery Challan', {
-	// refresh: function(frm) {
-	
-	// }
+    refresh: function(frm) {
+        frm.fields_dict['delivery_challan_item'].grid.get_field('quantity').get_query = function(doc, cdt, cdn) {
+            var d = locals[cdt][cdn];
+            return {
+                filters: {
+                    
+                }
+            };
+        };
+    }
 });
+frappe.ui.form.on('Delivery Challan Item', {
+    quantity: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        if (child.quantity) {
+            calculate_net_total(frm);
+            calculate_total_qty(frm);
+        }
+    },
+    rate: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        frappe.model.set_value(cdt, cdn, "amount", child.quantity * child.rate);
+        calculate_net_total(frm);
+        calculate_total_qty(frm);
+    }
+
+});
+function calculate_net_total(frm) {
+    let items = frm.doc.delivery_challan_item || [];
+    let totalAmount = 0;
+	let totalqty= 0;
+
+    for (let item of items) {
+		if(item.quantity && item.rate){
+        totalAmount += item.amount;
+		totalqty += item.quantity;
+		}
+    }
+	console.log("!!!!!!!!!!!!", totalqty)
+    frm.set_value("net_total", totalAmount);
+	frm.set_value("total_qty",totalqty)
+	frm.refresh_field("total_qty")
+    frm.refresh_field("net_total");
+	
+}
--- gatepass/gatepass/doctype/delivery_challan/delivery_challan.py
+++ gatepass/gatepass/doctype/delivery_challan/delivery_challan.py
@@ -2,7 +2,20 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+# from frappe.model.document import Document
 
-class DeliveryChallan(Document):
-	pass
+# class DeliveryChallan(Document):
+#     def before_save(self):
+#         total_amount = 0
+#         total_qty = 0
+
+#         for item in self.delivery_challan_item:
+#             item.amount = float(item.quantity) * float(item.rate)
+#             total_amount += item.amount
+#             total_qty += float(item.quantity)
+
+#         self.net_total = total_amount
+#         self.total_qty = total_qty
+
+
+            # print("----------------------",i.amount)
